4da1fd9adb19bfbb2202376b71833d3a8fa7f5b0
Farzain - zFz <32348150+farzain@users.noreply.github.com>
2017-10-28 08:24:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -249,55 +249,53 @@ $push = array(
 				array(
 				'type' => 'template',
 				'altText' => 'this is a carousel template',
-				'template' => {
+				'template' => array(
 					'type' => 'carousel',
 					'columns' => [
 						{
-							'thumbnailImageUrl' => 'https=>//example.com/bot/images/item1.jpg',
+							'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
 							'title' => 'this is menu',
 							'text' => 'description',
-							'actions' => [
-								{
+							'actions' => array(
 									'type' => 'postback',
 									'label' => 'Buy',
 									'data' => 'action=buy&itemid=111'
-								},
-								{
-									'type' => 'postback',
-									'label' => 'Add to cart',
-									'data' => 'action=add&itemid=111'
-								},
-								{
-									'type' => 'uri',
-									'label' => 'View detail',
-									'uri' => 'http=>//example.com/page/111'
-								}
-							]
-						},
-						{
-							'thumbnailImageUrl' => 'https=>//example.com/bot/images/item2.jpg',
-							'title' => 'this is menu',
-							'text' => 'description',
-							'actions' => [
-								{
+								),
+								array(
 									'type' => 'postback',
 									'label' => 'Buy',
-									'data' => 'action=buy&itemid=222'
-								},
-								{
+									'data' => 'action=buy&itemid=111'
+								),
+								array(
 									'type' => 'postback',
-									'label' => 'Add to cart',
-									'data' => 'action=add&itemid=222'
-								},
-								{
-									'type' => 'uri',
-									'label' => 'View detail',
-									'uri' => 'http=>//example.com/page/222'
-								}
-							]
+									'label' => 'Buy',
+									'uri' => 'http://example.com/page/111'
+								)
+							)
+						},
+						{
+							'thumbnailImageUrl' => 'https://example.com/bot/images/item1.jpg',
+							'title' => 'this is menu',
+							'text' => 'description',
+							'actions' => array(
+									'type' => 'postback',
+									'label' => 'Buy',
+									'data' => 'action=buy&itemid=111'
+								),
+								array(
+									'type' => 'postback',
+									'label' => 'Buy',
+									'data' => 'action=buy&itemid=111'
+								),
+								array(
+									'type' => 'postback',
+									'label' => 'Buy',
+									'uri' => 'http://example.com/page/111'
+								)
+							)
 						}
 					]
-				}
+				)
 }
 	
 }
